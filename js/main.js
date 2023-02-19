9669e6b85551858ca7d49a630b29b7f3c5032d5e
abdulloxprogrammer <abdulloxr3545@gmail.com>
2023-02-19 08:48:06 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ window.addEventListener("scroll", function () {
   }
 
   function openNavbar() {
-    document.getElementById("navbar-responsive").style.left = "0";
+    document.getElementById("navbar-responsive").style.right = "0";
     // document.getElementById("navbar-responsive").style.top = "0";
   }
   function closeNavbar() {
-    document.getElementById("navbar-responsive").style.left = "-100%";
+    document.getElementById("navbar-responsive").style.right = "-100%";
     // document.getElementById("navbar-responsive").style.top = "-100%";
   }
   
